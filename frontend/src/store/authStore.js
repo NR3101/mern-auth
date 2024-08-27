cff71248f9d87bca8d94f59bbf1747d72d27af81
Neeraj Rai <neeraj2000rai@gmail.com>
2024-08-27 18:55:36 +0530

    refactor: update API URL handling in authStore.js

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,7 +1,11 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth"; //Base URL of the API
+//Base URL of the API 
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5000/api/auth"
+    : "/api/auth"; 
 
 axios.defaults.withCredentials = true; //for allowing cookies to be sent with requests
 
